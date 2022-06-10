07c359d8efcc75893151b161746c7d2818b5da69
AugustWahlberg <aug.wahlberg@gmail.com>
2022-06-10 21:58:51 +0200

    .
    .

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ arrowDown.addEventListener("click", goDown)
 function goDown () {
   workSite.scrollIntoView({behavior: "smooth"});
 }
+
+document.getElementById("ff-web").onclick = function () {
+  location.href = "https://frontend-freakz.netlify.app/";
+};
+
+document.getElementById("ff-git").onclick = function () {
+  location.href = "https://github.com/Noroff-FEU-Assignments/project-exam-1-AugustWahlberg";
+};
