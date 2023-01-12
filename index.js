72ef899a73edff1861c70d73b282c59d25d26e88
AugustWahlberg <aug.wahlberg@gmail.com>
2023-01-12 04:28:57 +0100

    Added projects

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const arrowDown = document.getElementById("go-down")
-const workSite = document.getElementById("work")
+const workSite = document.getElementById("websites")
 
 arrowDown.addEventListener("click", goDown)
 
@@ -8,32 +8,42 @@ function goDown () {
 }
 
 document.getElementById("ff-web").onclick = function () {
-  location.href = "https://frontend-freakz.netlify.app/";
+  window.open ("https://frontend-freakz.netlify.app/", "_blank");
 };
 
 document.getElementById("ff-git").onclick = function () {
-  location.href = "https://github.com/Noroff-FEU-Assignments/project-exam-1-AugustWahlberg";
+  window.open ("https://github.com/Noroff-FEU-Assignments/project-exam-1-AugustWahlberg", "_blank");
 };
 
 
 document.getElementById("rd-web").onclick = function () {
-  location.href = "https://rainy-days-august-wahlberg.netlify.app/";
+  window.open ("https://rainy-days-august-wahlberg.netlify.app/", "_blank");
 };
 
 
 document.getElementById("rd-git").onclick = function () {
-  location.href = "https://github.com/Noroff-FEU-Assignments/cross-course-project-AugustWahlberg";
+  window.open ("https://github.com/Noroff-FEU-Assignments/cross-course-project-AugustWahlberg", "_blank");
 };
 
 
 document.getElementById("csm-web").onclick = function () {
-  location.href = "https://csm-august-wahlberg.netlify.app/";
+  window.open("https://csm-august-wahlberg.netlify.app/", "_blank");
 };
 
 document.getElementById("csm-git").onclick = function () {
-  location.href = "https://github.com/AugustWahlberg/Community-Science-Museum";
+  window.open("https://github.com/AugustWahlberg/Community-Science-Museum", "_blank");
 };
 
 document.getElementById("portfolio-git").onclick = function () {
-  location.href = "https://github.com/AugustWahlberg/Portfolio";
+  window.open("https://github.com/AugustWahlberg/Portfolio", "_blank");
+};
+
+
+document.getElementById("magazine-pdf").onclick = function () {
+  window.open("https://drive.google.com/file/d/128s88tWIddgY9EIR7-g9HUiE4xmE_4wI/view?usp=sharing", "_blank");
+};
+
+
+document.getElementById("poster-pdf").onclick = function () {
+  window.open("https://drive.google.com/file/d/1q4COKiNFA2xITJXteyfDtKE75wJhByvv/view?usp=sharing", "_blank");
 };
